baa2263e352640cef6f4c9a58d38daf54dd2658c
Rafael Cunha <rafaelcn1@hotmail.com>
2022-05-30 17:40:28 -0300

    Botões iniciais adicionados!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("Teste");
+var comecarJogar = function() {
+    var divDigiteUmaPalavra = document.getElementById("digiteUmaPalavra");
+    var digiteUmaPalavra = document.createElement("input");
+    digiteUmaPalavra.setAttribute("class", "digiteUmaPalavra");
+    digiteUmaPalavra.setAttribute("type", "text");
+    digiteUmaPalavra.setAttribute("name", "digiteUmaPalavra");
+    digiteUmaPalavra.setAttribute("placeholder", "Digite uma palavra ou frase");
+    divDigiteUmaPalavra.appendChild(digiteUmaPalavra);
+    removerBotoesIniciais();
+
+}
+
+var adicionarNovaPalavra = function() {
+    console.log("adicionarNovaPalavra");
+}
+
+
+var removerBotoesIniciais = function() {
+    var botaoComecarJogar = document.getElementById("botaoComecarJogar");
+    var botaoAdicionarNovaPalavra = document.getElementById("botaoAdicionarNovaPalavra");
+    botaoComecarJogar.remove();
+    botaoAdicionarNovaPalavra.remove();
+}
